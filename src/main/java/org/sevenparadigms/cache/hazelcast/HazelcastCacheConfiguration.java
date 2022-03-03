afd8fa4ce75004bcff08818b07eff16d0bf6835d
Lao Tsing <lao.tsing@outlook.com>
2022-03-03 08:49:33 +0300

    smart update

--- src/main/java/org/sevenparadigms/cache/hazelcast/HazelcastCacheConfiguration.java
+++ src/main/java/org/sevenparadigms/cache/hazelcast/HazelcastCacheConfiguration.java
@@ -7,12 +7,15 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.util.ObjectUtils;
 
-@EnableConfigurationProperties
+import java.util.Objects;
+
 @Configuration(proxyBeanMethods = false)
 public class HazelcastCacheConfiguration {
     @Bean
-    public HazelcastInstance hazelcastInstance(HazelcastProperties properties) {
+    public HazelcastInstance hazelcastInstance(Environment env) {
         Config config = new Config();
 
         config.getJetConfig().setEnabled(true);
@@ -21,15 +24,18 @@ public class HazelcastCacheConfiguration {
                 .setTcpIpConfig(new TcpIpConfig().setEnabled(true))
                 .setMulticastConfig(new MulticastConfig().setEnabled(true))));
 
+        var timeout = Objects.isNull(env.getProperty("timeoutMinutes")) ? "5" : env.getProperty("timeoutMinutes");
+        assert timeout != null;
         config.addMapConfig(new MapConfig("default")
-                .setTimeToLiveSeconds(properties.getTimeoutMinutes() * 60)
+                .setTimeToLiveSeconds(Integer.parseInt(timeout) * 60)
                 .setEvictionConfig(new EvictionConfig().setEvictionPolicy(EvictionPolicy.LFU)));
 
-        if (properties.getKubernetes()) {
+        if (Objects.equals(env.getProperty("kubernetes"), "true")) {
+            var name = Objects.isNull(env.getProperty("serviceName")) ? "dev" : env.getProperty("serviceName");
             config.getNetworkConfig().getJoin().setKubernetesConfig(new KubernetesConfig()
                     .setEnabled(true)
                     .setProperty("namespace", "default")
-                    .setProperty("service-name", properties.getServiceName()));
+                    .setProperty("service-name", name));
         }
         return Hazelcast.newHazelcastInstance(config);
     }
